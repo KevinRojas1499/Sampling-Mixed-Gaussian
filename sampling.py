e972b48534265cc2955d341c7a8f3d6bbb764a45
KevinRojas1499 <kevinrojas99@gmail.com>
2022-11-15 07:52:48 -0500

    Refactoring the code

--- sampling.py
+++ sampling.py
@@ -16,16 +16,10 @@ c = [1/2,1/6,1/3]
 means = [[0.5,0.5],[-15,15], [8,8]]
 variances = [[[1,0],[0,1]], [[5, -2],[-2,5]] , [[1, 2],[2,1]]]
 
-def beta(t):
-    return 20*t
 
-def drift(x,t):
-    return -beta(t)*x/2
-
-def diffusion(t):
-    return sqrt(beta(t))
-
-sde = sde_lib.SDE(100,1,beta=beta(1))
+sde = sde_lib.LinearSDE(beta=20)
+samplesBeforeFFT = torch.tensor(generateSamples.get_samples_from_mixed_gaussian(c,means,variances,num_samples))
+samples = torch.fft.fft(samplesBeforeFFT,norm="forward")
 
 device = 'cpu'
 score_function = model.Score(2)
@@ -41,34 +35,19 @@ train = True
 train = False
 
 
-def generate_samples(score_network: torch.nn.Module, nsamples: int) -> torch.Tensor:
-    x_t = torch.randn((nsamples, 2))
-    time_pts = torch.linspace(1, 0, 1000)
-    beta = lambda t: beta(t)
-    for i in range(len(time_pts) - 1):
-        t = time_pts[i]
-        dt = time_pts[i + 1] - t
-        score = score_network(x_t,t.expand(x_t.shape[0], 1)).detach()
-        tot_drift = drift(x_t,t) - diffusion(t)**2 * score
-        tot_diffusion = diffusion(t)
-
-        # euler-maruyama step
-        x_t = x_t + tot_drift * dt + tot_diffusion * torch.randn_like(x_t) * torch.abs(dt) ** 0.5
-    return x_t
-
 if train:
     errors = training.train(sde=sde, score_model=score_function,data=samples, number_of_steps=150001)
     plt.plot(np.linspace(1,len(errors),len(errors)),errors)
     plt.show()
 else:
-    generatedSamples = generate_samples(score_network=score_function, nsamples=1000)
+    generatedSamplesFFT = sde.generate_samples_reverse(score_network=score_function, nsamples=1000)
 
 
     ab = torch.ones(1000) / 1000
-    realPart = generatedSamples.real.type(torch.double)
+    realPart = generatedSamplesFFT.real.type(torch.double)
     M = ot.dist(samples,realPart, metric='euclidean')
     print(ot.emd2(ab,ab,M))
 
     plt.scatter(samples[:,0],samples[:,1], color='red')
-    plt.scatter(generatedSamples[:,0],generatedSamples[:,1])
+    plt.scatter(generatedSamplesFFT[:,0],generatedSamplesFFT[:,1])
     plt.show()
--- sde_lib.py
+++ sde_lib.py
@@ -2,28 +2,50 @@ import abc
 import torch
 
 class SDE(abc.ABC):
-  #In reality im doing this for a specific SDE
   def __init__(self, N,T, beta):
     # Right now I just assume thhe function is beta*t
     super().__init__()
     self.N = N
     self.T = T
+
+  @abc.abstractmethod
+  def marginal_prob_mean(self, x, t):
+    """Parameters to determine the mean of distribution of the SDE, $p_t(x)$."""
+    pass
+
+  @abc.abstractmethod
+  def marginal_prob_var(self, x, t):
+    """Parameters to determine the variance of the distribution of the SDE, $p_t(x)$."""
+    pass
+
+class LinearSDE(SDE):
+  
+  def __init__(self,beta):
+    self.f = lambda x,t : -beta*t*x/2
+    self.g = lambda t : (beta*t)**.5
     self.beta = beta
 
+
   def marginal_prob_mean(self, x0, t):
-    alpha = self.beta
-    var = torch.exp(-alpha*t*t/4)
+    var = torch.exp(-self.beta*t*t/4)
     return torch.mul(var,x0)
 
   def marginal_prob_var(self, t):
-    alpha = 20
-    var = torch.exp(-alpha*t*t/4)
+    var = torch.exp(-self.beta*t*t/4)
     return 1-var
     
 
-class ReverseSDE():
-  def __init__(self, sde,score):
-    self.N = sde.N
-    self.T = sde.T
-    self.f = lambda x,t : sde.f(x,t) - sde.g(t) * sde.g(t) * score(x, self.T - t)
-    self.g = sde.g
+  def generate_samples_reverse(self, score_network: torch.nn.Module, nsamples: int) -> torch.Tensor:
+      x_t = torch.randn((nsamples, 2))
+      time_pts = torch.linspace(1, 0, 1000)
+      beta = lambda t: beta(t)
+      for i in range(len(time_pts) - 1):
+          t = time_pts[i]
+          dt = time_pts[i + 1] - t
+          score = score_network(x_t,t.expand(x_t.shape[0], 1)).detach()
+          tot_drift = self.f(x_t,t) - self.g(t)**2 * score
+          tot_diffusion = self.g(t)
+
+          # euler-maruyama step
+          x_t = x_t + tot_drift * dt + tot_diffusion * torch.randn_like(x_t) * torch.abs(dt) ** 0.5
+      return x_t
