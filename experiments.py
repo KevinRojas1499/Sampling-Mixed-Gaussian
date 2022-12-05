eb3b0f3daf5c2b8db3095564495f9cbd1554896c
KevinRojas1499 <kevinrojas99@gmail.com>
2022-12-05 14:53:03 -0500

    Adding changes that include first steps of sampling from fourier space

--- experiments.py
+++ experiments.py
@@ -1,10 +1,32 @@
+import numpy as np 
 import torch 
-a = torch.tensor([[1,2],[3,4]])
-b = torch.tensor([[5,6],[7,8]])
 
-c = torch.cat((a,b),dim=1)
-print(c)
+dimension = 2
 
-d,e = torch.chunk(c,2,dim=1)
+def getJacobianOfFourierTransform(dimension):
+    jacobianOfFourierTransform = np.zeros((dimension,dimension))
+    for i in range(dimension):
+      for j in range(dimension):
+        if i%2 == 1 or i == 0:
+           jacobianOfFourierTransform[i,j] = np.cos(2*np.pi*i*j/dimension)
+        else:
+           jacobianOfFourierTransform[i,j] = np.sin(2*np.pi*i*j/dimension)
+    return jacobianOfFourierTransform
 
-print(d,e)
+print(getJacobianOfFourierTransform(3))
+
+t = torch.arange(6)
+print(t)
+ft = torch.fft.rfft(t)
+
+print(ft)
+a = []
+
+for c in ft:
+    a.append(torch.real(c))
+    if torch.imag(c).item() != 0 :
+        a.append(torch.imag(c))
+print(a)
+
+tt = torch.tensor([ 0.7626,  1.1540, -1.0174])
+print(torch.fft.irfft(tt,3))
--- sde_lib.py
+++ sde_lib.py
@@ -1,9 +1,10 @@
 import abc
 import torch
+import numpy as np
+
 
 class SDE(abc.ABC):
   def __init__(self):
-    # Right now I just assume thhe function is beta*t
     super().__init__()
 
   @abc.abstractmethod
@@ -34,17 +35,62 @@ class LinearSDE(SDE):
     return 1-var
     
 
-  def generate_samples_reverse(self, score_network: torch.nn.Module, nsamples: int) -> torch.Tensor:
-      x_t = torch.randn((nsamples, 4))
-      time_pts = torch.linspace(1, 0, 1000)
-      beta = lambda t: beta(t)
-      for i in range(len(time_pts) - 1):
-          t = time_pts[i]
-          dt = time_pts[i + 1] - t
-          score = score_network(x_t,t.expand(x_t.shape[0], 1)).detach()
-          tot_drift = self.f(x_t,t) - self.g(t)**2 * score
-          tot_diffusion = self.g(t)
+  def generate_samples_reverse(self, score_network: torch.nn.Module, dimension, nsamples: int) -> torch.Tensor:
+    device = 'cuda'
+    x_t = torch.randn((nsamples, dimension),device=device)
+    time_pts = torch.linspace(1, 0, 1000).to(device)
+    beta = lambda t: beta(t)
+    for i in range(len(time_pts) - 1):
+        t = time_pts[i]
+        dt = time_pts[i + 1] - t
+        score = score_network(x_t,t.expand(x_t.shape[0], 1)).detach()
+        tot_drift = self.f(x_t,t) - self.g(t)**2 * score
+        tot_diffusion = self.g(t)
 
-          # euler-maruyama step
-          x_t = x_t + tot_drift * dt + tot_diffusion * torch.randn_like(x_t) * torch.abs(dt) ** 0.5
-      return x_t
+        # euler-maruyama step
+        x_t = x_t + tot_drift * dt + tot_diffusion * torch.randn_like(x_t) * torch.abs(dt) ** 0.5
+    return x_t
+  
+  def generate_samples_reverse_fft(self, score_network: torch.nn.Module, dimension, nsamples: int) -> torch.Tensor:
+    # This score function is in the data space
+    device = 'cuda'
+    x_t = torch.randn((nsamples, dimension),device=device)
+    time_pts = torch.linspace(1, 0, 1000).to(device)
+    beta = lambda t: beta(t)
+
+    jacobianOfFourierTransform = torch.linalg.inv(getJacobianOfFourierTransform(dimension).to(device=device,dtype=torch.float))
+    # jacobianOfFourierTransform = getJacobianOfFourierTransform(dimension).to(device=device,dtype=torch.float)
+    
+    print(jacobianOfFourierTransform)
+    for i in range(len(time_pts) - 1):
+        t = time_pts[i]
+        dt = time_pts[i + 1] - t
+        
+        x_fourier_t = torch.tensor([ getTransform(x) for x in torch.fft.irfft(x_t,norm="forward",n=dimension).to(device=device) ]).to(device)
+        score = score_network(x_fourier_t,t.expand(x_fourier_t.shape[0], 1)).detach() @ jacobianOfFourierTransform
+
+        tot_drift = self.f(x_t,t) - self.g(t)**2 * score
+        tot_diffusion = self.g(t)
+
+        # euler-maruyama step
+        x_t = x_t + tot_drift * dt + tot_diffusion * torch.randn_like(x_t) * torch.abs(dt) ** 0.5
+    return x_t
+
+def getJacobianOfFourierTransform(dimension):
+  jacobianOfFourierTransform = torch.zeros((dimension,dimension))
+  for i in range(dimension):
+    for j in range(dimension):
+      if i%2 == 1 or i == 0:
+          jacobianOfFourierTransform[i,j] = np.cos(2*np.pi*i*j/dimension)
+      else:
+          jacobianOfFourierTransform[i,j] = np.sin(2*np.pi*i*j/dimension)
+  return jacobianOfFourierTransform
+
+def getTransform(ft):
+  a = []
+  for c in ft:
+    print(c)
+    a.append(torch.real(c))
+    if torch.imag(c).item() != 0 :
+        a.append(torch.imag(c))
+  return torch.tensor(a)
