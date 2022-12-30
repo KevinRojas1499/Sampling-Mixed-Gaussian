42095641b0b963bb3c56f1a1eed5ffe2cd6353da
KevinRojas1499 <kevinrojas99@gmail.com>
2022-12-30 12:41:34 -0500

    Update clean_fid.py
    Adding simple example for upscale fourier

--- diffusion/clean_fid.py
+++ diffusion/clean_fid.py
@@ -3,7 +3,7 @@ from datasets import load_dataset
 from PIL import Image
 import numpy as np
 import pywt
-
+import matplotlib.pyplot as plt
 
 # This is for computing fid using the clean fid library
 # names = ["local-unet-cifar10-32", "dual-fno-cifar10-32", "fno-cifar10-32",]
@@ -52,6 +52,26 @@ def upscaleFourier(image,zerosToPad):
         ffImg64[:,:,j] = b
     return ffImg64.real
 
+def upscaleFourier1D(image,zerosToPad):
+    n = image.shape[0]
+    nEnd = n  + 2*zerosToPad
+    ffImg64 = np.zeros(nEnd ,dtype='complex128')
+
+    a = np.fft.fft(image ,norm="backward")
+    a = np.fft.fftshift(a)
+
+    # b = np.random.randn(nEnd,nEnd).astype('complex128')
+    b = np.zeros(nEnd).astype('complex128')
+
+    b[zerosToPad:zerosToPad+n] = a
+
+    b = np.fft.ifftshift(b)
+    b = np.fft.ifft(b,norm="backward")
+    b*=(nEnd/n)
+
+    ffImg64 = b
+    return ffImg64.real
+
 
 # def upscaleWavelets(image,nStart,nEnd):
 #     ffImg64 = np.zeros((nEnd,nEnd,3),dtype='complex128')
@@ -74,6 +94,19 @@ def saveImage(im ,path):
     img = Image.fromarray(image,'RGB')
     img.save(path)
 
+
+def simpleExample():
+    L = 5
+    a = np.linspace(-L,L,100)
+    c = np.linspace(-L,L,200)
+
+    b = 1/(2+np.sin(a))
+
+    plt.plot(a,b)
+    plt.plot(c,upscaleFourier1D(b,50))
+    plt.show()
+
+simpleExample()
 
 for i, image in enumerate(dataset):
     if i>20: 
